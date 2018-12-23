30d9410a0206248fb9e280fa710fcce7feb2ea09
RAMESH BABU <RAMESH BABU@10.0.0.233>
2018-12-23 13:44:17 -0500

    ChangesFromBranch

--- Test2/src/com/ramesh/firstRepo/Test.java
+++ Test2/src/com/ramesh/firstRepo/Test.java
@@ -1,5 +1,24 @@
 package com.ramesh.firstRepo;
 
 public class Test {
+	public static void main(String[] args) {
+	
+		
+		System.out.println();
+		System.out.println("Test.changes()");
+		Test.changes();
+		
+	}
+	
+	public static void changes()
+	{
+		System.out.println("hello i am in branch");
+	System.out.println("hello switched using Eclipse");
+	}
 
+	
+	public static void branchMethod()
+	{
+		System.out.println("Branch method ");
+	}
 }
